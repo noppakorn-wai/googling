baa8ebc8f53e998fdc6a0eccddbe959567ff56c2
Noppakorn Chaiboonruang <waitingind@gmail.com>
2019-03-31 18:34:43 +0700

    chore: add firebase deployment script

---
+++ internal/webpack.production.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  mode: 'production',
+  entry: './services/functions.js',
+  target: 'node',
+  stats: 'errors-only',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+  ],
+  output: {
+    path: path.join(__dirname, '../.build'),
+    filename: 'index.js',
+    libraryTarget: 'this',
+  },
+}
